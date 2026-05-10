6f95506c6ff1e41494580428554409437ebc4c30
crazefool <ole.olivares@hotmail.com>
2026-05-10 22:50:44 +0200

    Revert "Door Lever"
    This reverts commit 70db979415fa32d60977ee7f9390aca1963b1aec.

--- Assets/Scripts/LeverTrigger.cs
+++ Assets/Scripts/LeverTrigger.cs
@@ -2,8 +2,8 @@ using UnityEngine;
 
 public class LeverTrigger : MonoBehaviour
 {
-    [Header("Object to Activate")]
-    [SerializeField] private GameObject targetObject;
+    [Header("Platform to Activate")]
+    [SerializeField] private GameObject platformToActivate;
 
     [Header("Optional: One-time use")]
     [SerializeField] private bool disableAfterUse = true;
@@ -17,20 +17,8 @@ public class LeverTrigger : MonoBehaviour
 
         hasTriggered = true;
 
-        if (targetObject != null)
-        {
-            // If it's a door
-            DoorOpener door = targetObject.GetComponent<DoorOpener>();
-            if (door != null)
-            {
-                door.OpenDoor();
-            }
-            else
-            {
-                // Default behavior: activate object (platform)
-                targetObject.SetActive(true);
-            }
-        }
+        if (platformToActivate != null)
+            platformToActivate.SetActive(true);
 
         if (disableAfterUse)
             gameObject.SetActive(false);
